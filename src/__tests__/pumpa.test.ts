3fd2d85bd4fdc079058f2f492a8d17e2760b6997
ivandotv <390700+ivandotv@users.noreply.github.com>
2022-04-22 11:54:32 +0200

    add tests for optional injection

---
+++ src/__tests__/pumpa.test.ts
@@ -0,0 +1,90 @@
+import { Pumpa } from '../pumpa'
+import { get } from '../utils'
+
+describe('Optional injection', () => {
+  test('Optional does not throw', () => {
+    const pumpa = new Pumpa()
+
+    class TestA {
+      static inject = [get('some_key', { optional: true })]
+
+      constructor(public optionalProp?: string) {}
+    }
+
+    pumpa.bindClass('class_a', TestA)
+
+    expect(() => pumpa.resolve<TestA>('class_a')).not.toThrow()
+  })
+  test('Optional injection resolves to undefined', () => {
+    const pumpa = new Pumpa()
+
+    class TestA {
+      static inject = [get('some_key', { optional: true })]
+
+      constructor(public optionalProp?: string) {}
+    }
+
+    pumpa.bindClass('class_a', TestA)
+    const instance = pumpa.resolve<TestA>('class_a')
+
+    expect(instance.optionalProp).toBeUndefined()
+  })
+
+  test('Optional injection at last position', () => {
+    const pumpa = new Pumpa()
+
+    const keyOne = 'key_one'
+    const valueOne = 1
+    const keyTwo = 'undefined_key'
+
+    class TestA {
+      static inject = [keyOne, get(keyTwo, { optional: true })]
+
+      constructor(
+        public keyOne: number,
+        public optionalProp?: string,
+        // @ts-expect-error for testing purposes
+        public keyThree: number
+      ) {}
+    }
+
+    pumpa.bindValue(keyOne, valueOne)
+    pumpa.bindClass('class_a', TestA)
+
+    const instance = pumpa.resolve<TestA>('class_a')
+
+    expect(instance.keyOne).toBe(valueOne)
+    expect(instance.optionalProp).toBeUndefined()
+  })
+
+  test('Optional injection can at in any position', () => {
+    const pumpa = new Pumpa()
+
+    const keyOne = 'key_one'
+    const valueOne = 1
+    const keyTwo = 'undefined_key'
+    const keyThree = 'key_two'
+    const valueThree = 2
+
+    class TestA {
+      static inject = [keyOne, get(keyTwo, { optional: true }), keyThree]
+
+      constructor(
+        public keyOne: number,
+        public optionalProp?: string,
+        // @ts-expect-error for testing purposes
+        public keyThree: number
+      ) {}
+    }
+
+    pumpa.bindValue(keyOne, valueOne)
+    pumpa.bindValue(keyThree, valueThree)
+    pumpa.bindClass('class_a', TestA)
+
+    const instance = pumpa.resolve<TestA>('class_a')
+
+    expect(instance.keyOne).toBe(valueOne)
+    expect(instance.optionalProp).toBeUndefined()
+    expect(instance.keyThree).toBe(valueThree)
+  })
+})
