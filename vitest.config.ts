4241b88a4846d5c63e4716271d2ea9253f927d87
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2023-12-22 16:50:30 +0100

    Add coverage exclude paths to vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html', 'clover', 'json']
+      reporter: ['text', 'html', 'clover', 'json'],
+      exclude: [
+        ...(configDefaults.coverage.exclude
+          ? configDefaults.coverage.exclude
+          : []),
+        'typedoc.cjs',
+        'src/types-internal.ts',
+        'src/types.ts',
+        'src/index.ts'
+      ]
     }
   }
 })
